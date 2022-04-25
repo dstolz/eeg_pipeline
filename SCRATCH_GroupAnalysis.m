eefdf6c0a7051c76da7e72e68c2e765211975ac8
daniel <daniel.stolzberg@gmail.com>
2022-04-25 10:29:27 -0400

    include pre/post conditinos

--- SCRATCH_GroupAnalysis.m
+++ SCRATCH_GroupAnalysis.m
@@ -1,8 +1,8 @@
 %% Group Analysis
 
-% EXP means that the participant is receiving training on recognizing time 
-% compressed sentences. ACT means that the participant is receiving training 
-% on recognizing sentences in noise.
+% EXP means that the participant is receiving training on recognizing time
+% compressed sentences.
+% ACT means that the participant is receiving training on recognizing sentences in noise.
 
 modelPathRoot = 'L:\Users\dstolz\EEGData_32\';
 
@@ -12,8 +12,8 @@ ForegroundOrBackground = "Foreground";
 % dataSuffix = 'DSS';
 dataSuffix = 'CLEAN';
 
-modelDirection = 1; % 1: forward model; -1: backwards model
-
+% modelDirection = 1; % 1: forward model; -1: backwards model
+modelDirection = -1;
 
 
 
@@ -29,12 +29,15 @@ fn = fieldnames(SD);
 for i = 1:length(fn)
     uSD.(fn{i}) = unique([SD.(fn{i})]);
 end
+uSD.Condition = ["Pre" "Post"];
 
 disp(uSD)
 
+fn = fieldnames(uSD);
+
+
 groupFn = setdiff(fn,{'Subject','status'});
 fprintf('Groups: %s\n',char(join(groupFn,', ')))
-
 
 
 % load model data
@@ -44,29 +47,30 @@ ffn = fullfile(modelPathRoot,fn);
 load(ffn,'model');
 
 subjIdx = 1;
+condIdx = 2;
 trainIdx = 4;
 
 badIdx = [];
 for i = 1:length(model)
-    [~,fnEEG,~] = fileparts(model{i}.fnEEG);
-    x = split(fnEEG,'_');
-    x = string(x);
-
-    xt = x(trainIdx);
     
-        
-    ind = [SD.Subject] == x(subjIdx); % & [SD.Train] == trainLUT(indLUT,2);
+    if isempty(model{i}) || ~isfield(model{i},'train')
+        badIdx = [badIdx, i];
+        continue
+    end
+    
+    info = model{i}.info;
+    
+    
+    
+    ind = [SD.Subject] == info(subjIdx); % & [SD.Train] == trainLUT(indLUT,2);
     
     model{i}.Subject = SD(ind).Subject;
     model{i}.MCI     = SD(ind).has_MCI;
     model{i}.Age     = SD(ind).Age;
     model{i}.Train   = SD(ind).Train;
     model{i}.status  = SD(ind).status;
-    model{i}.TCcond  = x(trainIdx);
-    
-    if ~isfield(model{i},'w')
-        badIdx = [badIdx, i];
-    end
+    model{i}.TCcond  = info(trainIdx);
+    model{i}.Condition = info(condIdx);
 end
 model(badIdx) = [];
 
@@ -74,36 +78,47 @@ Data = cell2mat(model);
 
 
 %
-f = figure(9999);
+f = figure(modelDirection+1000);
 clf(f)
 set(f,'color','w');
 hold on
 indBase = [Data.status] == "OK" & ~[Data.MCI];
-for i = 1:length(uSD.Train) % "ACT" or "EXP"
-    for j = 1:length(uSD.Age)
-        ind = indBase ...
-            & [Data.Train] == uSD.Train(i) ...
-            & [Data.Age] == uSD.Age(j);
-        
-        
-        if ~any(ind), continue; end
-        
-        str = sprintf('%s,%s,n=%d', ...
-            uSD.Age(j),uSD.Train(i),sum(ind));
-        
-        w = {Data(ind).w};
-        
-        w = cellfun(@(a) mean(a,3),w,'uni',0);
-        w = cell2mat(w');
-        mw = mean(w,1);
-        
-        t = model{find(ind,1)}.t;
-        
-        plot(t,mw,'DisplayName',str);
+for k = 1:length(uSD.Condition) % "Pre" or "Post"
+    for i = 1:length(uSD.Train) % "ACT" or "EXP"
+        for j = 1:length(uSD.Age)
+            ind = indBase ...
+                & [Data.Condition] == uSD.Condition(k) ...
+                & [Data.Train] == uSD.Train(i) ...
+                & [Data.Age] == uSD.Age(j);
+            
+            
+            if ~any(ind), continue; end
+            
+            str = sprintf('%s-%s,%s,n=%d', ...
+                uSD.Condition(k),uSD.Age(j),uSD.Train(i),sum(ind));
+            
+            %         w = {Data(ind).w};
+            %
+            %         w = cellfun(@(a) mean(a,3),w,'uni',0);
+            %         w = cell2mat(w');
+            %         mw = mean(w,1);
+            
+            train = [Data(ind).train];
+            
+            gfp = mean(cat(1,train.GFP));
+            
+            plot(train(1).t,gfp,'DisplayName',str);
+        end
     end
 end
 hold off
 legend
+ylabel('GFP (a.u.)')
+if modelDirection > 0
+    title('EEG - forward model')
+else
+    title('Stimulus - reverse model')
+end
 axis tight
 box on
 grid on
